db1e97f4d2854f206f15796ae7c1bdc8ecf6b0f4
Vlada1356277 <v.galimova.sas@gmail.com>
2023-05-25 00:03:36 +0300

    .

---
+++ components/episode-card.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export const EpisodesCard = ({ name, air_date, episode }) => {
+  return (
+    <div
+      style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+            border: '1px solid red',
+      }} >
+        <span>{name}</span>
+        <span>{air_date}</span>
+        <span>{episode}</span>
+    </div>
+  );
+};
